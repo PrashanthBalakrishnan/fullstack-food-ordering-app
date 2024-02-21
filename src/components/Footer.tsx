f5cb97b573960d3f8b636d8676a95c383295779f
Prashanth Balakrishnan <prashbala13@gmail.com>
2024-02-20 21:02:13 -0500

    ui complete

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,15 @@
+import Link from "next/link";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="h-12 md:h-24 p-4 lg:px-20 xl:px-40 text-red-500 flex items-center justify-between">
+      <Link href="/" className="font-bold text-xl">
+        MASSIMO
+      </Link>
+      <p className="text-sm">
+        &copy; {new Date().getFullYear()} Massimo. All rights reserved.
+      </p>
+    </div>
+  );
 };
 export default Footer;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,40 @@
 import Link from "next/link";
 import Menu from "./Menu";
+import CartIcon from "./CartIcon";
+import Image from "next/image";
 
 const Navbar = () => {
+  const user = false;
   return (
-    <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase">
+    <nav className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase md:h-24 lg:px-20 xl:px-40 ">
+      {/* LEFT LINKS */}
+      <div className="hidden md:flex gap-4 flex-1">
+        <Link href="/">Home</Link>
+        <Link href="/menu">Menu</Link>
+        <Link href="/">Contact</Link>
+      </div>
       {/* LOGO */}
-      <div className="text-xl">
+      <div className="text-xl md:font-bold flex-1 md:text-center">
         <Link href="/">MASSIMO</Link>
       </div>
       {/* Mobile menu */}
-      <div>
+      <div className="md:hidden">
         <Menu />
       </div>
-    </div>
+      {/* RIGHT LINKS */}
+      <div className="hidden md:flex gap-4 items-center flex-1 justify-end ">
+        <div className="flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md md:absolute top-3 lg:static">
+          <Image src="/phone.png" alt="phone icon" width={20} height={20} />
+          <span>123 456 7892</span>
+        </div>
+        {!user ? (
+          <Link href="/login">Login</Link>
+        ) : (
+          <Link href="/orders">Orders</Link>
+        )}
+        <CartIcon />
+      </div>
+    </nav>
   );
 };
 export default Navbar;
