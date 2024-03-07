8e79da87cb794790fcf24ee502b45f12ddc77253
Prashanth Balakrishnan <prashbala13@gmail.com>
2024-03-07 14:04:18 -0500

    changed get methods

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -2,40 +2,38 @@ import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/utils/prismadb";
 
 // FETCH ALL PRODUCTS
-
 export const GET = async (req: NextRequest) => {
   const { searchParams } = new URL(req.url);
   const cat = searchParams.get("cat");
+
   try {
     const products = await prisma.product.findMany({
       where: {
         ...(cat ? { catSlug: cat } : { isFeatured: true }),
       },
     });
-    return new NextResponse(JSON.stringify(products), {
-      status: 200,
-    });
-  } catch (error) {
-    return new NextResponse(JSON.stringify("Something went wrong!"), {
-      status: 500,
-    });
+    return new NextResponse(JSON.stringify(products), { status: 200 });
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }),
+      { status: 500 }
+    );
   }
 };
-
 // ADD PRODUCT
 export const POST = async (req: NextRequest) => {
   try {
     const body = await req.json();
-
-    const products = await prisma.product.create({
+    const product = await prisma.product.create({
       data: body,
     });
-    return new NextResponse(JSON.stringify(products), {
-      status: 201,
-    });
-  } catch (error) {
-    return new NextResponse(JSON.stringify("Something went wrong!"), {
-      status: 500,
-    });
+    return new NextResponse(JSON.stringify(product), { status: 201 });
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }),
+      { status: 500 }
+    );
   }
 };
